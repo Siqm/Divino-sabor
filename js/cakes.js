5a9e14f91b8a84d5996c55e6cf4a95495ecc7635
Siqm <rafaelsmello01@gmail.com>
2023-10-01 09:11:28 -0300

    [js] Inicio do JS e atualização no readme

---
+++ js/cakes.js
@@ -0,0 +1,82 @@
+export const bolos = [
+    {
+        titulo: "Bolo de Chocolate",
+        descricao: "Um bolo rico e indulgente feito com cacau de alta qualidade e coberto com ganache de chocolate meio amargo."
+    },
+    {
+        titulo: "Bolo de Morango",
+        descricao: "Um bolo leve e fofo recheado com camadas de creme de morango fresco e decorado com morangos inteiros."
+    },
+    {
+        titulo: "Bolo de Limão",
+        descricao: "Um bolo cítrico e refrescante com uma camada de cobertura de limão e raspas de limão para dar sabor."
+    },
+    {
+        titulo: "Bolo de Baunilha",
+        descricao: "Um bolo clássico de baunilha com um toque de baunilha natural, perfeito para todas as ocasiões."
+    },
+    {
+        titulo: "Bolo Red Velvet",
+        descricao: "Um bolo vermelho vibrante com creme de queijo na cobertura, conhecido por seu sabor único e textura macia."
+    },
+    {
+        titulo: "Bolo de Nozes",
+        descricao: "Um bolo denso e rico em nozes picadas, coberto com glacê de nozes e decorado com nozes inteiras."
+    },
+    {
+        titulo: "Bolo de Cenoura",
+        descricao: "Um bolo de cenoura úmido e delicioso com cobertura de cream cheese e pedaços de cenoura."
+    },
+    {
+        titulo: "Bolo de Coco",
+        descricao: "Um bolo tropical de coco com glacê de coco e flocos de coco torrado para dar textura."
+    },
+    {
+        titulo: "Bolo de Framboesa",
+        descricao: "Um bolo suave e recheado com geleia de framboesa fresca e coberto com framboesas inteiras."
+    },
+    {
+        titulo: "Bolo de Maçã",
+        descricao: "Um bolo de maçã caseiro com pedaços de maçã e canela, perfeito para o outono."
+    },
+    {
+        titulo: "Bolo de Maracujá",
+        descricao: "Um bolo de maracujá com uma camada de calda de maracujá doce e saborosa."
+    },
+    {
+        titulo: "Bolo de Pistache",
+        descricao: "Um bolo verde vibrante de pistache com cobertura de creme de pistache e pistaches picados."
+    },
+    {
+        titulo: "Bolo de Amendoim",
+        descricao: "Um bolo rico em amendoim com cobertura de manteiga de amendoim e pedaços de amendoim crocante."
+    },
+    {
+        titulo: "Bolo de Abacaxi",
+        descricao: "Um bolo de abacaxi úmido com pedaços de abacaxi e glacê de creme de abacaxi."
+    },
+    {
+        titulo: "Bolo de Café",
+        descricao: "Um bolo de café aromático com uma camada de streusel de nozes e uma xícara de café forte."
+    },
+    {
+        titulo: "Bolo de Menta",
+        descricao: "Um bolo refrescante de menta com cobertura de chocolate e folhas de menta fresca."
+    },
+    {
+        titulo: "Bolo de Laranja",
+        descricao: "Um bolo de laranja cítrico com uma camada de glacê de laranja e raspas de laranja para decorar."
+    },
+    {
+        titulo: "Bolo de Amora",
+        descricao: "Um bolo de amora repleto de amoras frescas e cobertura de chantilly."
+    },
+    {
+        titulo: "Bolo de Banana",
+        descricao: "Um bolo de banana úmido com pedaços de banana madura e uma pitada de canela."
+    },
+    {
+        titulo: "Bolo de Morango com Chocolate Branco",
+        descricao: "Um bolo de morango com camadas de chocolate branco e morangos frescos na cobertura."
+    }
+];
--- js/script.js
+++ js/script.js
@@ -1,39 +1,52 @@
-function carregarArquivo(arquivo, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", arquivo, true);
-    xhr.onload = function () {
-        if (xhr.status == 200) {
-            callback(xhr.responseText);
-            carregarEstilos(arquivo); // Carrega os estilos após carregar o HTML
-        }
-    };
-    xhr.send();
+document.addEventListener('DOMContentLoaded', function() {
+    smoothScroll();
+
+});
+
+function setupLoginForm() {
+    const loginForm = document.getElementById("login-form");
+    const errorMessage = document.getElementById("error-message");
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "usuario" && password === "senha") {
+        // Autenticação bem-sucedida
+        errorMessage.textContent = "Login bem-sucedido!";
+        errorMessage.style.color = "green";
+        // Redirecionar ou executar ação desejada após o login
+    } else {
+        // Autenticação falhou
+        errorMessage.textContent = "Usuário ou senha inválidos.";
+        errorMessage.style.color = "red";
+    }
 }
 
-function carregarEstilos(arquivo) {
-    // Determine o caminho do arquivo CSS com base no caminho do arquivo HTML
-    var caminhoCSS = arquivo.replace(".html", ".css");
+// document.getElementById('user-icon').addEventListener('click', redirect)
 
-    // Crie uma tag <link> para carregar o arquivo CSS
-    var link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = caminhoCSS;
+function smoothScroll() {
+    // Selecione todos os links <a> com href começando com #
+    console.log('chamou')
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        // Adicione um evento de clique para cada link
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault(); // Evite o comportamento de rolagem padrão
 
-    // Adicione a tag <link> ao <head> do documento
-    document.head.appendChild(link);
-}
+            const targetId = this.getAttribute('href').substring(1); // Obtenha o ID do alvo (removendo o #)
+            const targetElement = document.getElementById(targetId); // Encontre o elemento de destino
 
-function mostrarArquivos() {
-    var headerPath = '../components/header/index.html';
-    var mainPath = "../components/main/index.html";
-
-
-    carregarArquivo(headerPath, function (headerHTML) {
-        carregarArquivo(mainPath, function(mainHTML) {
-            var conteudo = document.getElementById("conteudo");
-            conteudo.innerHTML = headerHTML + mainHTML;
-        })
+            if (targetElement) {
+                // Role suavemente para o elemento alvo
+                const headerHeight = 128
+                window.scrollTo({
+                    top: targetElement.offsetTop - headerHeight, // Altura do elemento alvo em relação ao topo da página
+                    behavior: 'smooth' // Rola suavemente
+                });
+            }
+        });
     });
 }
 
-mostrarArquivos();
+function redirect() {
+    window.location.href = '../login'
+}
